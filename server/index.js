a37c6cb711343ec4f22b05fa70207b3f2b5ba914
Brian <144404297+knappbrian@users.noreply.github.com>
2026-04-19 23:42:30 -0400

    Use crypto.randomInt for secure shortcode generation
    replaced Math.random() with crypto.randomInt() for secure randomness and to avoid bias

--- server/index.js
+++ server/index.js
@@ -86,7 +86,7 @@ function generateShortCode(length = 6) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
     for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
+        result += characters.charAt(crypto.randomInt(0, characters.length));
     }
     return result;
 }
